049fa711ac759cb4f3b73d8489995503267a54e6
Nocsis <nicoslentzsch@web.de>
2018-11-12 11:09:35 +0100

    added 2) and 3) of Hausaufgabe 2

--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
@@ -12,23 +12,45 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	float time = ofGetElapsedTimef();
+
+	float yellowX = sin(time), yellowY = cos(time), brownX = cos(time), brownY = sin(time);
+
+	//Background update
+	if (ofGetMouseX() <= 500) { ofBackground(1, 0, 80); }//blau
+	else { ofBackground(226, 40, 27); }//rot
+
 	// kreis oben links
-	ofSetColor(247,198,18);//gelb
+	if (ofGetMouseX() <= 500) { ofSetColor(247, 198, 18); }//gelb
+	else { ofSetColor(52, 39, 30); }//braun
 	ofDrawCircle(0,0,450);
-	ofSetColor(52,39,30);//braun
+	
+	if (ofGetMouseX() <= 500) { ofSetColor(52, 39, 30); }//braun
+	else { ofSetColor(247, 198, 18); }//gelb
 	ofDrawCircle(0,0,225);
 
 	//dreieck
-	ofSetColor(226,40,27); //rot
+	if (ofGetMouseX() <= 500) { ofSetColor(226, 40, 27); }//rot
+	else { ofSetColor(1, 0, 80); }//blau
 	ofDrawTriangle(0,1000,300,700,550,1000);
+
+
 
 	//kreise unten
 	ofSetColor(119,132,140);//grau
 	ofDrawCircle(600,600,350); //statisch
 
-	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(678,498,220);
-	ofSetColor(52,39,30);//braun
+	ofPushMatrix();
+	ofTranslate(600, 600);
+
+	if (ofGetMouseX() <= 500) { ofSetColor(247, 198, 18); }//gelb
+	else { ofSetColor(52, 39, 30); }//braun
+	ofDrawCircle(0,0,220);
+
+	ofPopMatrix();
+
+	if (ofGetMouseX() <= 500) { ofSetColor(52, 39, 30); }//braun
+	else { ofSetColor(247, 198, 18); }//gelb
 	ofDrawCircle(712,449,130);
 
 	//rahmen
--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_eigenes_bild/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_eigenes_bild/src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofBackground(0); //blau
+	ofBackground(0); //schwarz
     ofSetCircleResolution(500);
 }
 
@@ -13,12 +13,35 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	float timeconstant = sin(ofGetElapsedTimef()); //Wert zwischen -1 und 1
+
 	// kreis oben links
 	ofVec2f circlePosition = ofVec2f(50,50);
+	ofVec2f mouse = ofVec2f(mouseX, mouseY);
+	int circleRadius = 30;
 
 	while (circlePosition.x < 1000) {
 			while (circlePosition.y < 1000) {
-				ofDrawCircle(circlePosition.x, circlePosition.y, 30);
+
+				if (timeconstant >= 0 && circlePosition.x == 370 && circlePosition.y == 370  || timeconstant >= 0 && circlePosition.x == 530 && circlePosition.y == 370 || timeconstant >= 0 && circlePosition.x == 370 && circlePosition.y == 530 || timeconstant >= 0 && circlePosition.x == 530 && circlePosition.y == 530) {
+					ofSetColor(0, 0, 255);
+				}
+				else if (timeconstant < 0 && circlePosition.x == 370 && circlePosition.y == 370 || timeconstant < 0 && circlePosition.x == 530 && circlePosition.y == 370 || timeconstant < 0 && circlePosition.x == 370 && circlePosition.y == 530 || timeconstant < 0 && circlePosition.x == 530 && circlePosition.y == 530) {
+					ofSetColor(255);
+				}
+				else if (timeconstant < 0 && circlePosition.x == 450 && circlePosition.y == 370 || timeconstant < 0 && circlePosition.x == 370 && circlePosition.y == 450 || timeconstant < 0 && circlePosition.x == 530 && circlePosition.y == 450 || timeconstant < 0 && circlePosition.x == 450 && circlePosition.y == 530) {
+					ofSetColor(0, 0, 255);
+				}
+				else if (fmod(circlePosition.y - 50, 160) == 0 && fmod(circlePosition.x - 50, 160) == 0) {
+					ofSetColor(0, 0, 255);
+					float distance = mouse.distance(circlePosition);
+
+				}
+				else {
+					ofSetColor(255);
+				}
+
+				ofDrawCircle(circlePosition.x, circlePosition.y, circleRadius);
 				circlePosition.y = circlePosition.y + 80;
 			}
 			circlePosition.x += 80;
