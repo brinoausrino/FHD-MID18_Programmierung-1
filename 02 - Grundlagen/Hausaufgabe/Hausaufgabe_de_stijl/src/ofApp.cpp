de2700a8c357adc455694c991471da96cf2f5162
Unknown <maltemon@web.de>
2018-11-12 12:17:30 +0100

    actually uploaded complete code

--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
@@ -2,45 +2,142 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofBackground(1,0,80); //blau
-    ofSetCircleResolution(500);
+	ofBackground(1, 0, 80); //blau
+	ofSetCircleResolution(500);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+
+	//t = ofGetElapsedTimef();
+
+	
+
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	// kreis oben links
-	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(0,0,450);
-	ofSetColor(52,39,30);//braun
-	ofDrawCircle(0,0,225);
+	float circlex = sin(ofGetElapsedTimef());
+	float circley = cos(ofGetElapsedTimef());
+
+	/*ofVec3f yellow(247, 198, 18);
+	ofVec3f red(226, 40, 27);
+	ofVec3f brown(52, 39, 30);
+	ofVec3f grey(119, 132, 148);*/
+
+
+	// kreise oben links
+	if (mouseX > 500)
+		ofSetColor(226, 40, 27);
+	else if (mouseY > 500)
+		ofSetColor(52, 39, 30);
+	else
+		ofSetColor(247, 198, 18);//gelb
+	ofDrawCircle(0, 0, 450);
+
+	if (mouseX > 500)
+		ofSetColor(247, 198, 18);
+	else if (mouseY > 500)
+		ofSetColor(226, 40, 27);
+	else
+		ofSetColor(52, 39, 30);//braun
+	ofDrawCircle(0, 0, 225);
 
 	//dreieck
-	ofSetColor(226,40,27); //rot
-	ofDrawTriangle(0,1000,300,700,550,1000);
+	if (mouseX > 500)
+		ofSetColor(52, 39, 30);
+	else if (mouseY > 500)
+		ofSetColor(247, 198, 18);
+	else
+		ofSetColor(226, 40, 27); //rot
+	ofDrawTriangle(0, 1000, 300, 700, 550, 1000);
 
 	//kreise unten
-	ofSetColor(119,132,140);//grau
-	ofDrawCircle(600,600,350); //statisch
+	ofSetColor(119, 132, 140);//grau
+	ofDrawCircle(600, 600, 350); //statisch
 
-	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(678,498,220);
-	ofSetColor(52,39,30);//braun
-	ofDrawCircle(712,449,130);
+	ofMatrix3x3;
+	ofTranslate(600, 600);
 
-	//rahmen
-	ofSetColor(0); //schwarz
-	ofDrawRectangle(0,0,1000,20);
-	ofDrawRectangle(0,0,20,1000);
-	ofDrawRectangle(0,980,1000,20);
-	ofDrawRectangle(980,0,20,1000);
+		ofMatrix3x3;
+		ofTranslate(circlex * 130, circley * 130);
+
+		if (mouseX > 500)
+			ofSetColor(226, 40, 27);
+		else if (mouseY > 500)
+			ofSetColor(52, 39, 30);
+		else
+			ofSetColor(247, 198, 18);//gelb
+		ofDrawCircle(0, 0, 220);
+
+			ofMatrix3x3;
+			ofTranslate(circlex * 85, circley * 85);
+
+			if (mouseX > 500)
+				ofSetColor(247, 198, 18);
+			else if (mouseY > 500)
+				ofSetColor(226, 40, 27);
+			else
+				ofSetColor(52, 39, 30);//braun
+			ofDrawCircle(0, 0, 130);
+
+			ofPopMatrix();
+		ofPopMatrix();
+	ofPopMatrix();
+	
 }
 
+//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	if (key == 's'){
-		ofSaveScreen("capture_" + ofGetTimestampString() + ".png");
-	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
 }
--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_eigenes_bild/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_eigenes_bild/src/ofApp.cpp
@@ -2,8 +2,8 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofBackground(0); //blau
-    ofSetCircleResolution(500);
+	ofSetBackgroundColor(100, 100, 255);
+	ofSetCircleResolution(500);
 }
 
 //--------------------------------------------------------------
@@ -13,31 +13,156 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	// kreis oben links
-	ofVec2f circlePosition = ofVec2f(50,50);
+	
+	/*ursprünglich sollte sich der "tag" anschalten, wenn man über den mond hovert,
+	aber aus testzwecken wird diese funktion lediglich der linken bildhälfte (x < 500) zugeordnet*/
 
-	while (circlePosition.x < 1000) {
-			while (circlePosition.y < 1000) {
-				ofDrawCircle(circlePosition.x, circlePosition.y, 30);
-				circlePosition.y = circlePosition.y + 80;
-			}
-			circlePosition.x += 80;
-			circlePosition.y = 50;
+	float t = ofGetElapsedTimef(); //zeit
+	int circlex = 730; //startpunkte des "regens"
+	int circley = 700;
+
+	//ofVec2f centerbig(300, 400);
+	//ofVec2f centersmall(350, 400);
+	ofVec2f center(290, 410); //zentrum der "kontrollsonne"
+	ofVec2f mouseposition(mouseX, mouseY);
+	//float distancebig = mouseposition.distance(centerbig);
+	//float distancesmall = mouseposition.distance(centersmall);
+	float distance = mouseposition.distance(center);
+	bool day = true;
+
+	if (distance < 350) {
+		day = true;
+	}
+	else {
+		day = false;
+	}
+
+		//himmel
+	if (mouseX > 500) {
+		ofSetColor(0); //schwarz
+	}
+	else {
+		ofSetColor(100, 100, 255); //himmelblau
+	}
+	ofDrawRectangle(0, 0, 1000, 1000);
+
+		//"kontrollsonne"
+	if (day = false) {
+		ofSetColor(15); //dunkelgrau
+	}
+	else {
+		ofSetColor(200, 20, 20); //rot
+	}
+	//ofDrawCircle(290, 410, 350);
+
+		//sonne_mond
+	if (mouseX > 500)
+		ofSetColor(210); //weiss
+	else
+		ofSetColor(220, 220, 30); //gelb
+	ofDrawCircle(300, 400, 300 + 35 * (sin(t * 4)));
+
+	if (mouseX > 500)
+		ofSetColor(120); //grau
+	else
+		ofSetColor(240, 40, 225); //pink
+	ofDrawCircle(350, 400, 230 + 25 * (cos(t * 4)));
+
+		//regen
+	ofSetColor(0, 30, 80); //dunkelblau
+	while (circlex < 930) {
+		circley = 700;
+		circlex = circlex + 30;
+		while (circley < 920) {
+			ofDrawCircle(circlex + (circley / 10) - 65 + rand() % 3, circley + mouseY % 20 + rand() % 3, 7);
+			circley = circley + 20;
 		}
+	}
 
-	//rahmen
-	ofSetColor(0); //schwarz
-	ofDrawRectangle(0,0,1000,20);
-	ofDrawRectangle(0,0,20,1000);
-	ofDrawRectangle(0,980,1000,20);
-	ofDrawRectangle(980,0,30,1000);
+		//wolke
+	ofSetColor(150, 150, 255); //hellblau
+	ofDrawCircle(800, 700, 60);
+	ofDrawCircle(850, 700, 60);
+	ofDrawCircle(900, 700, 60);
+	ofDrawCircle(825, 650, 60);
+	ofDrawCircle(875, 650, 60);
+
+		//boden
+	ofSetColor(80, 50, 0); //braun
+	ofDrawRectangle(600, 900, 400, 100);
+
+	ofSetColor(0, 150, 0); //grün
+	ofDrawRectangle(620, 900, 380, 30);
+
+		//blume
+	ofSetColor(0, 100, 0); //dunkleres grün
+	ofDrawRectangle(830, 800, 4, 102);
+
+	if (mouseX > 500) {
+		ofSetColor(0, 80, 0); //dunkelstes grün
+		ofDrawTriangle(820, 815, 847, 815, 827, 780);
+	}
+	else {
+		ofSetColor(210, 210, 30); //gelb
+		ofDrawCircle(832, 800, 35);
+	}
 
 	ofSetColor(255);
-	ofDrawBitmapString(titleArtPiece, 20, 985);
+
+	ofDrawBitmapString("Die Blume wird bei dem durchgehenden Regen bald ersaufen D:", 20, 985);
 }
 
+//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	if (key == 's'){
-		ofSaveScreen("capture_" + ofGetTimestampString() + ".png");
-	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
 }
