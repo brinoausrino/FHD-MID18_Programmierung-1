f95f377884414f5e70b73f4f7732b9328e68b74c
Unknown <blackbrowse95@gmail.com>
2018-11-23 17:47:05 +0100

    Hintergrund destijl geändert und animiert

--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
@@ -12,31 +12,52 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	if (mouseX < 500) {
+		ofSetColor(0,198,18);
+		ofDrawRectangle(20, 0, 960, 1000);
+
+	}
+
+
+
+	float t = ofGetElapsedTimef();
+	int x = sin(t)*40;
+	int y = sin(t) * 80;
+	int z = sin(t) * 100;
+	int c = cos(t) * 80;
+
+
+
 	// kreis oben links
 	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(0,0,450);
+	ofDrawCircle(c+0,c+0,450);
 	ofSetColor(52,39,30);//braun
-	ofDrawCircle(0,0,225);
+	ofDrawCircle(0,0,z+225);
 
 	//dreieck
 	ofSetColor(226,40,27); //rot
-	ofDrawTriangle(0,1000,300,700,550,1000);
+	ofDrawTriangle(0,x+1000,z+300,x+700,y+550,x+1000);
 
-	//kreise unten
-	ofSetColor(119,132,140);//grau
-	ofDrawCircle(600,600,350); //statisch
 
-	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(678,498,220);
-	ofSetColor(52,39,30);//braun
-	ofDrawCircle(712,449,130);
+		//kreise unten
+		ofSetColor(119, 132, 140);//grau
+		ofDrawCircle(y+600, y+600, 350); //statisch
 
-	//rahmen
-	ofSetColor(0); //schwarz
-	ofDrawRectangle(0,0,1000,20);
-	ofDrawRectangle(0,0,20,1000);
-	ofDrawRectangle(0,980,1000,20);
-	ofDrawRectangle(980,0,20,1000);
+		ofSetColor(247, 198, 18);//gelb
+		ofDrawCircle(z+678, y+498, x+220);
+		ofSetColor(52, 39, 30);//braun
+		ofDrawCircle(y+712, z+449, x+130);
+
+		//rahmen
+		ofSetColor(0); //schwarz
+		ofDrawRectangle(0, 0, 1000, 20);
+		ofDrawRectangle(0, 0, 20, 1000);
+		ofDrawRectangle(0, 980, 1000, 20);
+		ofDrawRectangle(980, 0, 20, 1000);
+
+		
+	
+	
 }
 
 void ofApp::keyPressed(int key){
