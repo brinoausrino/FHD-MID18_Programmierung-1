1f49221209484987ebfc6f77c06523453b5373ac
Montgomery Mews <seag1@gmx.de>
2018-11-23 18:18:21 +0100

    Hausaufgabe  de stijl

--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
@@ -1,5 +1,7 @@
 #include "ofApp.h"
 
+
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 	ofBackground(1,0,80); //blau
@@ -12,11 +14,22 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+
+	float t = ofGetElapsedTimef();
+	float temp = sin(t) * 25;
+
 	// kreis oben links
-	ofSetColor(255);//gelb
+	ofSetColor(247,198,18);//gelb
 	ofDrawCircle(0,0,450);
-	ofSetColor(52,39,30);//braun
-	ofDrawCircle(0,0,225);
+
+	if (mouseX > 500) {
+		ofSetColor(52, 39, 30);//braun
+		ofDrawCircle(0 + temp, 0 + temp, 225);
+	}
+	else {
+		ofSetColor(0, 150, 100);
+		ofDrawCircle(0 + temp, 0 + temp, 225);
+	}
 
 	//dreieck
 	ofSetColor(226,40,27); //rot
@@ -24,12 +37,12 @@ void ofApp::draw(){
 
 	//kreise unten
 	ofSetColor(119,132,140);//grau
-	ofDrawCircle(600,600,350); //statisch
+	ofDrawCircle(600,600,350*(temp/25)); 
 
 	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(678,498,220);
+	ofDrawCircle(678+temp,498+temp*15,220);
 	ofSetColor(52,39,30);//braun
-	ofDrawCircle(712,449,130);
+	ofDrawCircle(712+temp*15,449,130);
 
 	//rahmen
 	ofSetColor(0); //schwarz
@@ -37,6 +50,15 @@ void ofApp::draw(){
 	ofDrawRectangle(0,0,20,1000);
 	ofDrawRectangle(0,980,1000,20);
 	ofDrawRectangle(980,0,20,1000);
+
+	if (mouseX > 500) {
+		ofSetColor(52, 39, 30);//braun
+		ofDrawCircle(250 + temp, 250 + temp, 50);
+	}
+	else {
+		ofSetColor(0, 150, 100);
+		ofDrawCircle(250 + temp, 250 + temp, 50);
+	}
 }
 
 void ofApp::keyPressed(int key){
