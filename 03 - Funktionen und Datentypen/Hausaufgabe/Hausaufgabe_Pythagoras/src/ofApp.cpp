0d60edc66a9f7a5277a08af292af4598c87e5679
Montgomery Mews <seag1@gmx.de>
2018-11-23 13:13:35 +0100

    Hausaufgabe Pythagoras

--- 03 - Funktionen und Datentypen/Hausaufgabe/Hausaufgabe_Pythagoras/src/ofApp.cpp
+++ 03 - Funktionen und Datentypen/Hausaufgabe/Hausaufgabe_Pythagoras/src/ofApp.cpp
@@ -1,17 +1,39 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-ofBackground(255);
-ofSetCircleResolution(200);
+void ofApp::setup() {
+	ofBackground(255);
+	ofSetCircleResolution(200);
 
-int ak = 5;
-int gk = 3;
+	int ak = 7;
+	int gk = 8;
 
-int h = hypothenuse(ak,gk);
+	float h = hypothenuse(ak, gk);
 
-cout << "Die Hypothenuse der Katheten " << ak <<" und " << gk << " ist " << h <<endl;
+	float Ankathete[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	float Hypothenuse[] = { 11,12,13,14,15,16,17,18,19,20 };
 
+
+
+	cout << "Die Hypothenuse der Katheten " << ak << " und " << gk << " ist " << h << endl;	
+
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+
+	for (int i = 0; i < 10; i++) {
+		
+		cout << "Ankathete:		" << Ankathete[i] << "	" << "Hypothenuse:	"  << Hypothenuse[i] << endl;
+		cout << "Gegenkathete:		" << Kathete(Ankathete[i], Hypothenuse[i]) << endl;
+		cout << " " << endl;
+	}
+
+
+	
+
+
+		
 }
 
 //--------------------------------------------------------------
@@ -27,7 +49,17 @@ void ofApp::draw(){
 
 float ofApp::hypothenuse(float ankathete, float gegenkathete)
 {
+	float hypothenuse = sqrt((ankathete)*(ankathete)+(gegenkathete)*(gegenkathete));
 
+	return hypothenuse;
 }
 
 
+float ofApp:: Kathete(float Ankathete, float Hypothenuse)
+{
+	float return_wert;
+
+	return_wert = sqrt((Hypothenuse)*(Hypothenuse)-(Ankathete)*(Ankathete));
+
+	return return_wert;
+}
