7b0932654afa6b8f083ed091f93c2d6296638f34
Pierre <pierre@global-rm.com>
2018-11-26 00:04:11 +0100

    Hausaufgabe stijl Bild

--- 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
+++ 02 - Grundlagen/Hausaufgabe/Hausaufgabe_de_stijl/src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofBackground(1,0,80); //blau
+	ofBackground(1, 0, 80); //blau
     ofSetCircleResolution(500);
 }
 
@@ -12,11 +12,58 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+
+	float t = ofGetElapsedTimef();
+	int x = (sin(t) * 40);
+
+	if (mouseX < 300) {
+
+
+		ofSetBackgroundColor(0);
+	else {
+		ofGetBackgroundColor(167);
+	}
+
+	}
+
+
+	if (mouseX > 450) {
+
+		ofSetColor(255);
+		ofDrawCircle(200 + x, 200 + x, 100 + x);
+	}
+	else
+	{
+		
+		ofSetColor(119, 132, 140);
+		ofDrawCircle(200 + x, 200 + x, 100 + x);
+	}
+	
+
+
+	
+
+
+
+	
+
 	// kreis oben links
-	ofSetColor(0);//gelb
-	ofDrawCircle(0,0,450);
-	ofSetColor(52,39,30);//braun
-	ofDrawCircle(0,0,225);
+	ofSetColor(0);//schwarz
+	ofDrawCircle(100,100,50);
+
+	if (mouseX > 450) {
+
+		ofSetColor(52, 39, 30);//braun
+		ofDrawCircle(x * 10, x * 10, x * 3);
+	}
+	else
+	{
+
+		ofSetColor(198, 90, 15);//braun
+		ofDrawCircle(x * 10, x * 10, x * 3);
+	}
+
+	
 
 	//dreieck
 	ofSetColor(226,40,27); //rot
@@ -24,12 +71,23 @@ void ofApp::draw(){
 
 	//kreise unten
 	ofSetColor(119,132,140);//grau
-	ofDrawCircle(600,600,350); //statisch
+	ofDrawCircle(x*50,600,350); //statisch
 
-	ofSetColor(247,198,18);//gelb
-	ofDrawCircle(678,498,220);
+
+	if (mouseY > 500) {
+
+		ofSetColor(247, 198, 18);//gelb
+		ofDrawCircle(678, 498, x * 5);
+	}
+	else
+	{
+
+		ofSetColor(226, 40, 27);//gelb
+		ofDrawCircle(678, 498, x * 5);
+	}
+	
 	ofSetColor(52,39,30);//braun
-	ofDrawCircle(712,449,130);
+	ofDrawCircle(712,x*(-5),x*2);
 
 	//rahmen
 	ofSetColor(0); //schwarz
